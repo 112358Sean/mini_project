4b79415733240338174918ff6e8c8c3bc128ad5c
112358Sean <seanthony99@gmail.com>
2023-05-12 08:30:13 +0700

    perbaikan transaction

--- models/transaction-model.go
+++ models/transaction-model.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Transaction struct {
 	gorm.Model
+	ID_Transaksi 	string `json:"id_transaksi" form:"id_transaksi"`
 	ID_Keranjang	string `json:"id_keranjang" form:"id_keranjang"`
 	Status			string `json:"status" form:"status"`
 }
